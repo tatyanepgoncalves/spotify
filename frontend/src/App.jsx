9bc73215338748326d74e4d109ba7de57026d8b3
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-16 19:45:07 -0300

    :wrench: chore: update config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-
+import { Header } from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+     <Header />
+     <Main />
+    </>
+  );
+};
 
-export default App
+export default App;
