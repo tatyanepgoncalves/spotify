13cc4727dbabab18a07798e042d1ff1b691e628a
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-16 21:51:51 -0300

    🔧 chore: update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { Header } from "./components/Header";
-import Main from "./components/Main";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Artist from "./pages/Artist";
+import Artists from "./pages/Artists";
+import Songs from "./pages/Songs";
+import Song from "./pages/Song";
 
 const App = () => {
   return (
     <>
      <Header />
-     <Main />
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artist:id" element={<Artist />} />
+        <Route path="/songs" element={<Songs />} />
+        <Route path="/song:id" element={<Song />} />
+      </Routes>
     </>
   );
 };
